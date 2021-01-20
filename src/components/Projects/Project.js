3f75299574c27b8bea920c1c593bc1dbf71f53e9
Aman <55349238+X-genius@users.noreply.github.com>
2021-01-20 18:03:41 +0530

    update project

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -3,6 +3,7 @@ import RHImage from "../../images/blood_donation.jpg";
 import FarmerImage from "../../images/Farmer-website.jpg";
 import SweetImage from "../../images/sweet-shop-image.jpg";
 import AutomationImage from "../../images/Automation-Testing.jpg";
+import BlogWebsite from "../../images/BlogWebsite.jpg";
 import "./Project.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -22,10 +23,6 @@ const Project = () => {
             data-aos="fade-up-left"
             data-aos-offset="50"
           >
-            {/* <a href="https://rhpositive.herokuapp.com/">
-                  <img src={RHImage} className="rhImage" alt="imageRh" />
-                </a> */}
-
             <div class="flip-box">
               <div class="flip-box-inner">
                 <div class="flip-box-front">
@@ -51,14 +48,6 @@ const Project = () => {
             data-aos="zoom-in"
             data-aos-offset="50"
           >
-            {/* <a href="https://x-genius.github.io/KrishiUdhaan/">
-              <img
-                src={FarmerImage}
-                className="farmerImage"
-                alt="imageKrishi"
-              />
-            </a> */}
-
               <div class="flip-box">
               <div class="flip-box-inner">
                 <div class="flip-box-front">
@@ -86,10 +75,6 @@ const Project = () => {
             data-aos="fade-up-left"
             data-aos-offset="50"
           >
-            {/* <a href="https://x-genius.github.io/sweet/">
-              <img src={SweetImage} className="sweetImage" alt="imageSweet" />
-            </a> */}
-
               <div class="flip-box">
               <div class="flip-box-inner">
                 <div class="flip-box-front">
@@ -118,14 +103,6 @@ const Project = () => {
             data-aos="zoom-in"
             data-aos-offset="50"
           >
-            {/* <a href="https://github.com/X-genius?tab=repositories">
-              <img
-                src={AutomationImage}
-                className="automationImage"
-                alt="imageAuto"
-              />
-            </a> */}
-
              <div class="flip-box">
               <div class="flip-box-inner">
                 <div class="flip-box-front">
@@ -152,18 +129,41 @@ const Project = () => {
                 </div>
                </div>
              </div>
-            
-          </div>
-        </div>
-        {/* 
-                <div className = "rowProject">
-                    <div className = "col-12-Project" data-aos = "zoom-in" data-aos-offset = "50">
-                    <a href = "https://github.com/X-genius?tab=repositories">
-                            <img src = {AutomationImage} className = "automationImage" alt = "imageAuto"/>
-                        </a>
-                    </div>
-                </div> */}
+             </div>
+
+             <div
+            className="col-4-Project"
+            data-aos="zoom-in"
+            data-aos-offset="50"
+          >
+             <div class="flip-box">
+              <div class="flip-box-inner">
+                <div class="flip-box-front">
+                <img
+                src={BlogWebsite}
+                className="BlogImage"
+                alt="imageAuto"
+              />
+                </div>
+
+                <div class="flip-box-back">
+                  <div>
+                  <h2>Blog Website</h2>
+                  <p>This website based on mern stack, This website provide interface for the blogger to write his/her blog.
+                  </p>
+                  <div className = "LinksOfProject">
+                  <a href="https://github.com/X-genius/FullStackBloggingWebsite">
+                    view
+                 </a>
+                </div>
+                </div>
+                </div>
+               </div>
+             </div>
+             </div>
+
       </div>
+    </div>
     </div>
   );
 };
