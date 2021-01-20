2b3abb0f189093bd9d52200ac18379d27d880332
Aman <55349238+X-genius@users.noreply.github.com>
2021-01-20 16:45:50 +0530

    update project folder

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -22,44 +22,129 @@ const Project = () => {
             data-aos="fade-up-left"
             data-aos-offset="50"
           >
-            <a href="https://rhpositive.herokuapp.com/">
-              <img src={RHImage} className="rhImage" alt="imageRh" />
-            </a>
+            {/* <a href="https://rhpositive.herokuapp.com/">
+                  <img src={RHImage} className="rhImage" alt="imageRh" />
+                </a> */}
+
+            <div class="flip-box">
+              <div class="flip-box-inner">
+                <div class="flip-box-front">
+                  <img src={RHImage} className="rhImage" alt="imageRh" />
+                </div>
+
+                <div class="flip-box-back">
+                  <h2>RH+</h2>
+                  <p>This Project help for needy person who want to blood and minimum cost of time.</p>
+                  <div className = "LinksOfProject">
+                  <a href="https://rhpositive.herokuapp.com/" >
+                    view
+                </a>
+                </div>
+                </div>
+               </div>
+             </div>
           </div>
           <div
             className="col-4-Project"
             data-aos="zoom-in"
             data-aos-offset="50"
           >
-            <a href="https://x-genius.github.io/KrishiUdhaan/">
+            {/* <a href="https://x-genius.github.io/KrishiUdhaan/">
               <img
                 src={FarmerImage}
                 className="farmerImage"
                 alt="imageKrishi"
               />
-            </a>
+            </a> */}
+
+              <div class="flip-box">
+              <div class="flip-box-inner">
+                <div class="flip-box-front">
+                <img src={FarmerImage} className="farmerImage" alt="imageKrishi"/>
+                </div>
+
+                <div class="flip-box-back">
+                  <h2>KrishiUdhaan</h2>
+                  <p>This Project based on farming culture, This project help to remove intermediators and farmer sell his/her crop
+                    at actual money.
+                  </p>
+                  <div className = "LinksOfProject">
+                  <a href="https://x-genius.github.io/KrishiUdhaan/">
+                     view
+                 </a> 
+                </div>
+                </div>
+               </div>
+             </div>
           </div>
           <div
             className="col-4-Project"
             data-aos="fade-up-left"
             data-aos-offset="50"
           >
-            <a href="https://x-genius.github.io/sweet/">
+            {/* <a href="https://x-genius.github.io/sweet/">
               <img src={SweetImage} className="sweetImage" alt="imageSweet" />
-            </a>
+            </a> */}
+
+              <div class="flip-box">
+              <div class="flip-box-inner">
+                <div class="flip-box-front">
+                <img src={SweetImage} className="sweetImage" alt="imageSweet" />
+                </div>
+
+                <div class="flip-box-back">
+                  <h2>My Sweet Shop</h2>
+                  <p>This project provide simple and decent sweet shop look.
+                  </p>
+                  <div className = "LinksOfProject">
+                  <a href="https://x-genius.github.io/sweet/">
+                   view
+                 </a> 
+                </div>
+                </div>
+               </div>
+             </div>
+
+
           </div>
           <div
             className="col-4-Project"
             data-aos="zoom-in"
             data-aos-offset="50"
           >
-            <a href="https://github.com/X-genius?tab=repositories">
+            {/* <a href="https://github.com/X-genius?tab=repositories">
               <img
                 src={AutomationImage}
                 className="automationImage"
                 alt="imageAuto"
               />
-            </a>
+            </a> */}
+
+             <div class="flip-box">
+              <div class="flip-box-inner">
+                <div class="flip-box-front">
+                <img
+                src={AutomationImage}
+                className="automationImage"
+                alt="imageAuto"
+              />
+                </div>
+
+                <div class="flip-box-back">
+                  <h2>Automation Projects</h2>
+                  <p>Provide github Link , In this Link three automation project available. These automation projects are
+                    Automailer , Automated Piano , youtube to mp3 converter.
+                    
+                  </p>
+                  <div className = "LinksOfProject">
+                  <a href="https://github.com/X-genius?tab=repositories">
+                    view
+                 </a>
+                </div>
+                </div>
+               </div>
+             </div>
+            
           </div>
         </div>
         {/* 
