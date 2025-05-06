ed993715b0de2fe8171e9f254bf3aa4dc2b60742
Aman Baghel <amanbaghel@Amans-MacBook-Pro.local>
2025-05-06 13:12:46 +0530

    Update About, Image and Resume

--- src/components/About Us/aboutUs.js
+++ src/components/About Us/aboutUs.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import myImage from '../../images/Aman_Image.jpg';
-import './aboutUs.css';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import React from "react";
+import myImage from "../../images/Aman_Image.jpg";
+import "./aboutUs.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const aboutUs = () => {
   Aos.init({
@@ -10,45 +10,45 @@ const aboutUs = () => {
     offset: 150,
   });
   return (
-    <div id='About'>
-      <div className='aboutContainer'>
-        <h1 className='aboutUS' data-aos='flip-left'>
+    <div id="About">
+      <div className="aboutContainer">
+        <h1 className="aboutUS" data-aos="flip-left">
           About Us
         </h1>
 
-        <div className='rowContainer'>
-          <div className='colFourContainer'>
+        <div className="rowContainer">
+          <div className="colFourContainer">
             <img
               src={myImage}
-              className='imgContainer'
-              alt='portfolioImage'
-              data-aos='flip-up'
+              className="imgContainer"
+              alt="portfolioImage"
+              data-aos="flip-up"
             />
 
             <h2
-              className='nameStyle'
-              data-aos='fade-up'
-              data-aos-anchor-placement='bottom-bottom'
+              className="nameStyle"
+              data-aos="fade-up"
+              data-aos-anchor-placement="bottom-bottom"
             >
               Aman Baghel
             </h2>
           </div>
 
-          <div className='colEightContainer'>
+          <div className="colEightContainer">
             <p
-              className='IntroParagraph'
-              data-aos='fade-right'
-              data-aos-offset='300'
-              data-aos-easing='ease-in-sine'
+              className="IntroParagraph"
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
             >
               I'm Aman and I have completed my Bachelor of Technology from Guru
-              Tegh Bahadur Institute of Technology with a commendable CGPA of{' '}
-              <strong>9.32</strong>. I am passionate about{' '}
-              <strong>Frontend Development</strong> with over{' '}
-              <strong>3+ years of experience</strong> as a{' '}
-              <strong>Software Engineer</strong>, I am currently employed at{' '}
-              <strong>Blue Yonder</strong> in the role of{' '}
-              <strong>Software Engineer</strong>.{' '}
+              Tegh Bahadur Institute of Technology with a commendable CGPA of{" "}
+              <strong>9.32</strong>. I am passionate about{" "}
+              <strong>Frontend Development</strong> with over{" "}
+              <strong>3.5+ years of experience</strong> as a{" "}
+              <strong>Software Engineer</strong>, I am currently employed at{" "}
+              <strong>Blue Yonder</strong> in the role of{" "}
+              <strong>Software Engineer</strong>.{" "}
             </p>
           </div>
         </div>
